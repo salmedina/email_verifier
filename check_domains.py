19ec80480de4aace3269a87e1b0dfcc8242b88ca
salmedina <salbed.medina@gmail.com>
2020-03-14 19:55:39 -0600

    Major reformat and additional logging

--- check_domains.py
+++ check_domains.py
@@ -1,45 +1,86 @@
 import argparse
-import pyping
+import ping3
+from tqdm import tqdm
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--email_lst', type=str,
                         help='Path to the file with the email list')
-    parser.add_argument('--domain_lst', type=str, 
+    parser.add_argument('--malformed_lst', type=str, default='malformed_emails.txt',
+                        help='Path to the output file with the malformed emails')
+    parser.add_argument('--valid_domain_lst', type=str, default='valid_domains.txt',
                         help='Path to the output file with the domain list')
-    parser.add_argument('--output_path', type=str,
+    parser.add_argument('--invalid_domain_lst', type=str, default='invalid_domains.txt',
+                        help='Path to the output fiel with the invalid domain list')
+    parser.add_argument('--output_path', type=str, default='valid_emails.txt',
                         help='Path to the output file with the verified domains')
     args = parser.parse_args()
     return args
 
+def get_domain(email):
+    return email[email.find('@')+1:]
 
 def main(args):
-    email_list = [l.strip() for l in open(args.email_lst, 'r').read_lines()]
-    print(f'Total emails in file:   {len(email_list)}')
-    
-    email_list = [e for e in email_list if e.find('@') != -1]
-    print(f'Total emails with @:    {len(email_list)}')
+    email_list = [l.strip() for l in open(args.email_lst, 'r').readlines()]
+    print(f'Total emails in file:        {len(email_list)}')
 
-    domain_set = set([e[e.find('@')+1:] for e in email_list])
-    print(f'Total domains found:    {len(domain_set)}')
+    wellformed_emails = list()
+    malformed_emails = list()
+    domain_list = list()
+    for email in tqdm(email_list):
+        if len(email) < 1 or \
+            email.find('@') == -1 or \
+            email[email.find('@')+1:][0] == ' ' or \
+            email[email.find('@')+1:][0] == '.' or \
+            email[email.find('@')+1:].find('..') != -1:
+            malformed_emails.append(email)
+        else:
+            wellformed_emails.append(email)
+            domain_list = get_domain(email)
+
+    with open(args.malformed_lst, 'w') as malformed_file:
+        malformed_file.write('\n'.join(malformed_emails))
+        print(f'Wrote malformed emails to {args.malformed_lst}')
+
+    print(f'Total well-formed emails:    {len(wellformed_emails)}')
+
+    domain_set = set(map(get_domain, wellformed_emails))
+    print(f'Total domains found:         {len(domain_set)}')
+
+
+    exit
 
     existing_domain_list = list()
-    for domain in domain_set:
-        r = pyping.ping(domain)
-        if r.ret_code == 0:
+    nonexisting_domain_list = list()
+    pbar = tqdm(domain_set)
+    for domain in pbar:
+        pbar.set_description(f'Pinging {domain}')
+        if ping3.ping(domain) != False:
             existing_domain_list.append(domain)
-    print(f'Total existing domains: {len(existing_domain_list)}')
+        else:
+            nonexisting_domain_list.append(domain)
+    print(f'Total valid domains:          {len(existing_domain_list)}')
+    print(f'Total invalid domains:        {len(nonexisting_domain_list)}')
 
-    with open(args.domain_lst, 'w') as domains_file:
+    with open(args.valid_domain_lst, 'w') as domains_file:
         domains_file.write('\n'.join(existing_domain_list))
+        print(f'Wrote valid domains to {args.valid_domain_lst}')
 
+    with open(args.invalid_domain_lst, 'w') as domains_file:
+        domains_file.write('\n'.join(nonexisting_domain_list))
+        print(f'Wrote valid domains to {args.invalid_domain_lst}')
+
+    print(f'Filtering emails with valid domains')
     existing_domain_email_list = list()
-    for email in email_list:
+    pbar = tqdm(email_list)
+    for email in pbar:
+        pbar.set_description(f'{email}')
         if email[email.find('@')+1:] in existing_domain_list:
             existing_domain_email_list.append(email)
     
     with open(args.output_path, 'w') as emails_file:
         emails_file.write('\n'.join(existing_domain_email_list))
+        print(f'Wrote valid domain emails to {existing_domain_email_list}')
     
     print(f'Finished processing emails.')
     
